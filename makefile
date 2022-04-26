dfdc7468081ab84528ccaf405852a5192f9c4bf5
Vinicius José Fritzen <www.vini84200@hotmail.com>
2022-04-25 21:33:56 -0300

    Tratando os argumentos

--- makefile
+++ makefile
@@ -1,17 +1,10 @@
 # Generated with: https://solver.assistedcoding.eu/makefilegen
 #
 
-OBJS	= src/ABP/main.o src/file/file.o src/AVL/main.o
-OUT	= out/parafraseABP,out/parafraseAVL
-
-OBJS0	= out/ABP/main.o out/file/file.o out/ABP/abp.o out/profiling/profiling.o
-SOURCE0	= src/ABP/main.c src/file/file.c
-HEADER0	= src/ABP/main.h src/file/file.h
+OBJS0	= out/ABP/main.o out/file/file.o out/ABP/abp.o out/profiling/profiling.o out/argParser.o
 OUT0	= out/parafraseABP
 
-OBJS1	= out/AVL/main.o out/file/file.o out/profiling/profiling.o
-SOURCE1	= src/AVL/main.c src/file/file.c
-HEADER1	= src/AVL/main.h src/file/file.h
+OBJS1	= out/AVL/main.o out/file/file.o out/profiling/profiling.o out/argParser.o
 OUT1	= out/parafraseAVL
 
 CC	 = gcc
@@ -42,6 +35,9 @@ out/file/file.o: src/file/file.c
 
 out/profiling/profiling.o: src/profiling/profiling.c src/profiling/profiling.h
 	$(CC) $(FLAGS) src/profiling/profiling.c -o out/profiling/profiling.o
+
+out/argParser.o: src/argParser.c src/argParser.h
+	$(CC) $(FLAGS) src/argParser.c -o out/argParser.o
 
 out/AVL/main.o: src/AVL/main.c
 	$(CC) $(FLAGS) src/AVL/main.c -o out/AVL/main.o
---
+++ src/argParser.c
@@ -0,0 +1,63 @@
+/*
+ * argParser.c
+ * Distributed under terms of the MIT license.
+ */
+
+#include "argParser.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+ParsedArgs parseArgs(int argc, char *argv[]) {
+    // Os args estão com um a mais pois o nome do arquivo tbm é 
+    // considerado um argumento
+    if (argc < 4 || argc > 5) {
+        printf("Número de argumentos inválido. Use 3 ou 4 argumentos\n " 
+                "Uso:\n"
+                "\t parafraseABP/AVL dict.txt in.txt out.txt [profile.txt]\n");
+        exit(1);
+        return (ParsedArgs) {
+            PARSE_ERROR,
+            NULL,
+            NULL,
+            NULL,
+            NULL
+        };
+    }
+
+
+    ParsedArgs res = {
+        PARSE_SUCCESS,
+        argv[1],
+        argv[2],
+        argv[3],
+        argc == 5 ? argv[4] : NULL
+    };
+
+
+    return res;
+
+}
+
+int file_exists(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
+
+void verificarArquivos(ParsedArgs arquivos) {
+    if (!file_exists(arquivos.dicionario_path)) {
+        printf("O dicionario \"%s\" não existe.\n", arquivos.dicionario_path);
+        exit(2);
+    }
+    if (!file_exists(arquivos.input_path)) {
+        printf("O arquivo de entrada \"%s\" não existe.\n", arquivos.input_path);
+        exit(2);
+    }
+
+    // TODO: Verificar se o arquivo out pode ser escrito
+    
+    return;
+}
